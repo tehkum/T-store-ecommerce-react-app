bbeba57491eb4f90bef2bf38acaa7536ead9212e
tehkum kanchwala <tehkumkanchwala@gmail.com>
2023-05-26 18:58:12 +0530

    loading components

---
+++ src/components/LoadingCard.jsx
@@ -0,0 +1,12 @@
+import "./loadinCard.css";
+
+export default function LoadingCard() {
+
+  return (
+    <div className="loading-container">
+      <div className="loading-img"></div>
+      <div className="title-loading"></div>
+      <div className="title-loading"></div>
+    </div>
+  );
+}
---
+++ src/components/LoadingCartCard.jsx
@@ -0,0 +1,15 @@
+import "./LoadingCart.css";
+
+export default function LoadingCartCard(){
+
+  return (
+    <div className="cart-load-card">
+      <div className="cart-load-img"></div>
+      <div className="cart-load-part">
+          <div className="cart-load-title"></div>
+          <div className="cart-load-para"></div>
+          <div className="cart-load-para"></div>
+      </div>
+    </div>
+  );
+}
