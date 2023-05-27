3d93ad71565192e4374cd93b6f9d8f31fc482ea2
tehkum kanchwala <tehkumkanchwala@gmail.com>
2023-05-27 13:16:37 +0530

    wishlist button error fixes

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,18 +1,26 @@
 import "./ProductCard.css";
 import { Link } from "react-router-dom";
-import h2 from "../images/h2.png";
+import light from "../images/h2.png";
+import dark from "../images/h1.png";
 import { useContext, useState } from "react";
 import { useWishlist } from "../context/WishlistProvider";
 
 export default function ProductCard({ id, image, title, price, category }) {
   const [showButton, setButton] = useState(false);
 
-  const { wishlistDispatch } = useContext(useWishlist);
+  const { wishlistDispatch, wishlistState } = useContext(useWishlist);
 
-  const clickHandler = async () => {
+  const clickAddHandler = async () => {
     setButton(true);
     setTimeout(async ()=> {
       await wishlistDispatch({type: "add-to-wishlist", data: data});
+      setButton(false);
+    }, 1500)
+  }
+  const clickRemoveHandler = async () => {
+    setButton(true);
+    setTimeout(async ()=> {
+      await wishlistDispatch({type: "remove-from-wishlist", data: data});
       setButton(false);
     }, 1500)
   }
@@ -24,6 +32,9 @@ export default function ProductCard({ id, image, title, price, category }) {
     price: price,
     category: category
   }
+  
+
+  const wishButton = wishlistState?.mainWish?.find(({_id})=>_id=== data._id) ? <img src={dark} alt="..." className="wish-logo" width="20px" height="20px" onClick={clickRemoveHandler} /> : <img src={light} alt="..." className="wish-logo" width="20px" height="20px" onClick={clickAddHandler} /> ;
 
   return (
     <div className="product-container">
@@ -31,7 +42,8 @@ export default function ProductCard({ id, image, title, price, category }) {
       <Link to={`/product/${id}`} >
       <img className="img-display" src={image} alt={title} />
        </Link>
-      {showButton ? <img src={h2} alt="..." className="wish-logo" width="20px" height="20px"/> : <img src={h2} alt="..." className="wish-logo" width="20px" height="20px" onClick={clickHandler} />} 
+      {/* {showButton ? <img src={h2} alt="..." className="wish-logo" width="20px" height="20px"/> : <img src={h2} alt="..." className="wish-logo" width="20px" height="20px" onClick={clickHandler} />}  */}
+      {showButton ? <></> : wishButton}
       <p className="price-product">{price}</p>
       </div>
       <p className="title-product">{title}</p>
