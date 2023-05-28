e066a565365f0cdcfcba188885fa53167a93f2cf
tehkum kanchwala <tehkumkanchwala@gmail.com>
2023-05-28 13:39:18 +0530

    nullish coelation added

--- src/components/AlertBox.jsx
+++ src/components/AlertBox.jsx
@@ -5,9 +5,9 @@ export default function AlertBox({alertMessage, clicked}){
     const [ showAlert, setAlert ] = useState(false);
 
     useEffect(()=>{
-        alertMessage.length && setAlert(true);
+        alertMessage?.length && setAlert(true);
         setTimeout(()=>setAlert(false),3000)
-    },[clicked, alertMessage.length])
+    },[clicked, alertMessage?.length])
 
 
     return<div className={showAlert ? "alert-box alert-show" : "alert-box alert-hidden"}>
