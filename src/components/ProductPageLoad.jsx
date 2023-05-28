ca6e7642e466f503f004a319137e8334f23d5d15
tehkum kanchwala <tehkumkanchwala@gmail.com>
2023-05-28 13:39:53 +0530

    loading state for individual products

---
+++ src/components/ProductPageLoad.jsx
@@ -0,0 +1,19 @@
+import "./ProductPageLoad.css";
+
+export default function ProductPageLoad(){
+    return <div className="product-page-loader">
+        <div className="product-page-loader1">
+            <div className="product-img-load"></div>
+            <div className="product-img-load"></div>
+            <div className="product-img-load"></div>
+            <div className="product-img-load"></div>
+        </div>
+        <div className="product-page-loader2">
+        <div className="product-title-load"></div>
+        <div className="product-des-load"></div>
+        <div className="product-des-load"></div>
+        <div className="product-des-load"></div>
+        <div className="product-des-load"></div>
+        </div>
+    </div>
+}
